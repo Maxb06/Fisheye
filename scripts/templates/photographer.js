20df65b687e3833288280453c2845a1fa015d4dc
Maxb06 <maxime.brunet@orange.fr>
2024-04-04 22:42:02 +0200

    style : page accueil + initialisation fetch

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,5 +1,5 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, tagline, city, country, price, portrait } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -9,9 +9,18 @@ function photographerTemplate(data) {
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        const p = document.createElement( 'p' );
+        p.textContent = city + ' , ' + country;
+        const pTagline = document.createElement( 'p' );
+        pTagline.textContent = tagline;
+        const pPrice = document.createElement( 'p' );
+        pPrice.textContent = price + '€/jour';
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(p);
+        article.appendChild(pTagline);
+        article.appendChild(pPrice)
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, tagline, city, country, price, picture, getUserCardDOM }
 }
