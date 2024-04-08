cfb8524980336f445ea1d52e164977b7754d873f
Maxb06 <maxime.brunet@orange.fr>
2024-04-08 16:40:39 +0200

    style : intégration page accueil / accessibilité

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -7,17 +7,21 @@ function photographerTemplate(data) {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
+        // ajout de l'attribut 'alt' avec le nom du photographe test
+        img.setAttribute('alt', `Portrait de ${name}`);
+        // ajout attribut aria-label nom et localisation du photographe test
+        img.setAttribute('aria-label', `Portrait de ${name}, photographe basé à ${city}, ${country}`);
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
-        const p = document.createElement( 'p' );
-        p.textContent = city + ' , ' + country;
+        const pLocations = document.createElement( 'p' );
+        pLocations.textContent = city + ' , ' + country;
         const pTagline = document.createElement( 'p' );
         pTagline.textContent = tagline;
         const pPrice = document.createElement( 'p' );
         pPrice.textContent = price + '€/jour';
         article.appendChild(img);
         article.appendChild(h2);
-        article.appendChild(p);
+        article.appendChild(pLocations);
         article.appendChild(pTagline);
         article.appendChild(pPrice)
         return (article);
