72325f59cbf0b19fd371bc501f265c1ae315b046
Maxb06 <maxime.brunet@orange.fr>
2024-04-18 11:26:11 +0200

    style : navigation page accueil et photographe + jsdoc

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,6 +1,12 @@
+    /**
+    * Récupère les données des photographes depuis un fichier JSON.
+    * @async
+    * @returns {Promise<{ photographers: Object[] }>} Les données des photographes.
+    */
     async function getPhotographers() {
         try {
             const response = await fetch('data/photographers.json');
+            console.log(response);
             if (!response.ok) throw new Error('Impossible de récupérer les données photographers');
             return await response.json();
         } catch (error) {
@@ -9,16 +15,31 @@
         }
     }
 
+    /**
+    * Affiche les données des photographes sur la page.
+    * @param {Object[]} photographers - Les données des photographes à afficher.
+    */
     async function displayData(photographers) {
         const photographersSection = document.querySelector(".photographer_section");
 
         photographers.forEach((photographer) => {
+            console.log(photographer);
             const photographerModel = photographerTemplate(photographer);
             const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
+
+            const link = document.createElement('a');
+            link.href = `photographer.html?id=${photographer.id}`; // Utilise l'ID du photographe dans l'url
+            link.appendChild(userCardDOM);
+            
+            photographersSection.appendChild(link);
         });
     }
-
+ 
+    /**
+    * Initialise l'application en récupérant et affichant les données des photographes.
+    * Gère les éventuelles erreurs d'initialisation.
+    * @async
+    */
     async function init() {
         try {
         // Récupère les datas des photographes
@@ -30,4 +51,5 @@
     }
     
     init();
+
   
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,15 +1,33 @@
+/**
+ * Modèle de photographe.
+ * @typedef {Object} PhotographerModel
+ * @property {string} name - Le nom du photographe.
+ * @property {string} tagline - La phrase d'accroche du photographe.
+ * @property {string} city - La ville du photographe.
+ * @property {string} country - Le pays du photographe.
+ * @property {number} price - Le prix du photographe par jour.
+ * @property {string} picture - Le chemin vers la photo du photographe.
+ * @property {Function} getUserCardDOM - Fonction pour obtenir la carte DOM du photographe.
+ */
+/**
+ * Crée un modèle de photographe à partir des données fournies.
+ * @param {Object} data - Les données du photographe.
+ * @returns {PhotographerModel} Le modèle de photographe.
+ */
 function photographerTemplate(data) {
     const { name, tagline, city, country, price, portrait } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
+    /**
+     * Obtient la carte DOM du photographe.
+     * @returns {HTMLElement} La carte DOM du photographe.
+     */
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        // ajout de l'attribut 'alt' avec le nom du photographe test
+        img.setAttribute("src", picture);
         img.setAttribute('alt', `Portrait de ${name}`);
-        // ajout attribut aria-label nom et localisation du photographe test
         img.setAttribute('aria-label', `Portrait de ${name}, photographe basé à ${city}, ${country}`);
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
@@ -23,7 +41,7 @@ function photographerTemplate(data) {
         article.appendChild(h2);
         article.appendChild(pLocations);
         article.appendChild(pTagline);
-        article.appendChild(pPrice)
+        article.appendChild(pPrice);
         return (article);
     }
     return { name, tagline, city, country, price, picture, getUserCardDOM }
